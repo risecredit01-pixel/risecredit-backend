935920e44057fbaeb0511461922c9f5d07c11ed6
risecredit01-pixel <risecredit01pixel@gmail.com>
2026-05-22 20:00:25 +0530

    feat: Add all 50 states to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,14 @@ import { FaPinterestP } from 'react-icons/fa';
 import './Footer.css';
 
 const states = [
-  'Alabama','California','Delaware','Idaho','Illinois','Indiana',
-  'Iowa','Kansas','Kentucky','Louisiana','Michigan','Mississippi',
-  'Missouri','Nevada','New Mexico','Ohio','Oklahoma','Rhode Island',
-  'South Carolina','Tennessee','Texas','Utah','Wisconsin','Wyoming'
+  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 
+  'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 
+  'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 
+  'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 
+  'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 
+  'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
 ];
 
 function Footer() {
